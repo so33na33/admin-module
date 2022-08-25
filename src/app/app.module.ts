bac8f19172b204d9d7da81e07ae0480d871b5cbd
so33na33 <santhoshsona150@gmail.com>
2022-08-26 00:33:30 +0530

    sidebar_admin

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { EventsComponent } from './events/events.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    EventsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
