bcb17b76170f9f767e3192090c429025910c5605
so33na33 <santhoshsona150@gmail.com>
2022-08-26 19:49:24 +0530

    sidebar fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { EventsComponent } from './events/events.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    EventsComponent
+
   ],
   imports: [
     BrowserModule,
